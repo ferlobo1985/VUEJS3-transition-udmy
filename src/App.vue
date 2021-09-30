6efc8fc8e5e0d9bb31d0c718276f5ff65cda7984
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-30 00:05:10 -0300

    07.1-transitions group template

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,19 @@
   <app-header></app-header>
         <div class="container">
             <!-- <compTransitionsOne></compTransitionsOne> -->
-            <comp-transitions-two></comp-transitions-two>
+            <comp-transitions-three></comp-transitions-three>
         </div>
     <app-footer></app-footer>
 </template>
 
 <script>
   // import compTransitionsOne from './components/Transitions/one';
-  import compTransitionsTwo from './components/Transitions/two.vue';
+  // import compTransitionsTwo from './components/Transitions/two.vue';
+    import compTransitionsThree from './components/Transitions/three.vue';
 
   export default{
     components:{
-      compTransitionsTwo
+      compTransitionsThree
     }
   }
 </script>
---
+++ src/components/Transitions/three.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="form-group">
+        <input v-model="name" class="form-control">
+    </div>
+    <button 
+        class="btn btn-primary" 
+        @click="addOne"
+    >
+        Add one
+    </button>    
+    <hr/>
+    <div>
+        <ul class="pl-0">
+
+                <li
+                    class="list-group-item"
+                    v-for="(item) in list"
+                    :key="item"
+                >
+                    {{ item }}
+                </li>
+  
+        </ul>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        data(){
+            return{
+                list:['Francis','Ron','Martha'],
+                name:''
+            }
+        },
+        methods:{
+            addOne(){
+          
+            },
+        }
+    }
+</script>
